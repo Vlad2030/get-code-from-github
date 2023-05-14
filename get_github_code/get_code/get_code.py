49cf628542194744e8c45fa03f90041b488286f9
vladdd0 <vlad123123v@gmail.com>
2023-05-14 19:10:33 +0300

    Added docstrings

--- get_github_code/get_code/get_code.py
+++ get_github_code/get_code/get_code.py
@@ -6,6 +6,24 @@ from get_github_code.request import Request
 
 
 class GetCode(Request, JSON, Decode):
+    """### GetCode class
+
+        Args:
+            `user (str)`: GitHub Username
+
+            `repo (str)`: GitHub repository name
+
+            `branch (str)`: Github repository branch
+
+            `file_path (str)`: File path in repository
+
+        functions:
+            `(property) get_code -> str:`
+                Return github code from file_path
+
+            `(property) async_get_code -> str:`
+                Asynchronous return github code from file_path
+    """
     def __init__(
             self,
             user: str,
@@ -22,15 +40,18 @@ class GetCode(Request, JSON, Decode):
 
     @property
     def get_code(self) -> str:
-        response = self.request(url=self.url)._body.decode("utf-8")
+        """Return github code from file_path"""
+
+        response = self.request(url=self.url)
         if not response.status == 200:
             raise GetCodeException("Connection refused")
-        json = self.loads(data=response)["content"]
-        return self.b64(data=json)
+        return self.loads(data=response._body.decode("utf-8"))["content"]
 
     @property
     async def async_get_code(self) -> str:
-        response = await self.async_request(url=self.url)._body.decode("utf-8")
+        """Asynchronous return github code from file_path"""
+
+        response = await self.async_request(url=self.url)
         if not response.status == 200:
             raise GetCodeException("Connection refused")
         json = self.loads(data=response)["content"]
@@ -43,12 +64,41 @@ def get_code(
         file_path: str,
         branch: str = "main",
 ) -> str:
+    """Get github code from repository
+
+        Args:
+            `user (str)`: GitHub Username
+
+            `repo (str)`: GitHub repository name
+
+            `branch (str)`: Github repository branch. Defaults to `"main"`
+
+            `file_path (str)`: File path in repository
+
+    Returns:
+        str: Code from github repository
+    """
     return GetCode(user, repo, branch, file_path).get_code
+
 
 async def async_get_code(
         user: str,
         repo: str,
         file_path: str,
         branch: str = "main",
-):
+) -> str:
+    """Asynchronous get github code from repository
+
+        Args:
+            `user (str)`: GitHub Username
+
+            `repo (str)`: GitHub repository name
+
+            `branch (str)`: Github repository branch. Defaults to `"main"`
+
+            `file_path (str)`: File path in repository
+
+    Returns:
+        str: Code from github repository
+    """
     return await GetCode(user, repo, branch, file_path).async_get_code
