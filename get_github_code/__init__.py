16775947cccf4a8db8122954099f2472a87b6763
vladdd0 <vlad123123v@gmail.com>
2023-05-14 19:10:03 +0300

    fix import

--- get_github_code/__init__.py
+++ get_github_code/__init__.py
@@ -1,24 +1,7 @@
-from get_github_code.get_code import get_code, async_get_code
+from get_github_code.get_code import GetCode, get_code, async_get_code
 
 __all__ = [
+    "GetCode",
     "get_code",
     "async_get_code",
 ]
-
-'''
-from get_github_code.json import JSON
-from get_github_code.get_code import GetCode
-from get_github_code.constants import API_URL
-from get_github_code.decode import Decode
-from get_github_code.exceptions import GetCodeException
-from get_github_code.request import Request
-
-__all__ = [
-    "JSON",
-    "API_URL",
-    "Decode",
-    "GetCodeException",
-    "GetCode",
-    "Request",
-]
-'''
